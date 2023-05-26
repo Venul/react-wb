8117bcda52c4dcfc3d94175d3e9210f0d195f04e
Sergey Lunev <slunev@eccentex.com>
2023-05-26 08:29:21 +0300

    Add MainPage cmp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainPage from './components/MainPage/MainPage.jsx'
+
+function App() {
+  return (
+    <div className="App">
+        <p>Hell World</p>
+        <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './MainPage.module.css'
+
+
+const MainPage = props => {
+
+return (
+    <div className={classes.mainPage}>
+        <p>Hell world</p>
+    </div>    
+    )
+}
+
+export default MainPage
