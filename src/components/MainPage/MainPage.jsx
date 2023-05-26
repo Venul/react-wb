eb83be6caa28e2ec744070ef6bb997b0e5bc4585
Sergey Lunev <slunev@eccentex.com>
2023-05-27 00:03:41 +0300

    add ts module, add basic form

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import classes from './MainPage.module.css'
+import React, { useState } from 'react'
+import classes from './MainPage.module.scss'
 
 
-const MainPage = props => {
+function MainPage() {
+    const [inputValue, setInputValue] = useState('');
 
-return (
-    <div className={classes.mainPage}>
-        <p>Hell world</p>
-    </div>    
-    )
+    const handleInputChange = (event) => {
+        debugger
+        setInputValue(event.target.value);
+    };
+
+    const onSearchGoogs = () => {
+        debugger
+        console.log(`Input value: ${inputValue}`);
+    };
+
+    return (
+        <div className={classes.mainPage}>
+            <p>Вставьте ссылку на товар</p>
+            <div>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder='https://www.wildberries.ru/catalog/157876315/detail.aspx'
+                />
+
+                <button onClick={onSearchGoogs}>Искать!</button>
+            </div>
+        </div>
+    );
 }
 
 export default MainPage
